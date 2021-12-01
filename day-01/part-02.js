aed640209b0238fd74b445f1124a6e256e8f96e8
Rowan Manning <accounts+github@rowanmanning.com>
2021-12-01 09:51:20 +0000

    Day 1 answers

--- day-01/part-02.js
+++ day-01/part-02.js
@@ -3,7 +3,14 @@ import readInputFileIntegers from '../shared/lib/read-input-file-integers.js';
 
 const input = await readInputFileIntegers(import.meta);
 
-const result = input.reduce((state, depth) => {
+const depths = input.reduce((chunks, value, index) => {
+	if (input[index + 2] !== undefined) {
+		chunks.push(value + input[index + 1] + input[index + 2]);
+	}
+	return chunks;
+}, []);
+
+const result = depths.reduce((state, depth) => {
 	if (state.previousDepth && state.previousDepth < depth) {
 		state.count += 1;
 	}
